1b30795214639cb4f82570769851c59548b72fde
Ayush MIshra <ayushh7066@gmail.com>
2025-06-28 13:13:37 +0530

    automatically redirecting to home-page for signedin users

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,6 +20,9 @@ import {Form,FormField
   // FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.actions";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -51,12 +54,42 @@ const formSchema = authFormSchema(type);
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try{
         if(type === 'sign-up'){
-          toast.success('Account created successfully. Please sign in.');
+        const { name, email, password } = values;
+
+        const userCredentials = await createUserWithEmailAndPassword(auth,email,password)
+         
+        const result = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+        
+        toast.success('Account created successfully. Please sign in.');
           router.push('/sign-in')
         } else {
+          const { email , password }=values;
+          const userCredentials = await signInWithEmailAndPassword(auth,email,password)
+          const idToken = await userCredentials.user.getIdToken();
+          if(! idToken){
+            toast.error('Sign in failed')
+            return;
+          }
+          await signIn({
+            email, idToken
+          })
+
+
+
+
           toast.success('Sign in successfully.');
           router.push('/')        }
 
--- firebase/client.ts
+++ firebase/client.ts
@@ -1,26 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { getApp, getApps, initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth} from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCUN70GCv6Vw8m8SaRndEeFT6lHtJnideg",
+  authDomain: "prepwise-df359.firebaseapp.com",
+  projectId: "prepwise-df359",
+  storageBucket: "prepwise-df359.firebasestorage.app",
+  messagingSenderId: "638456688582",
+  appId: "1:638456688582:web:c3e32b902db02eae47ccc7",
+  measurementId: "G-BLP8Y0HB7N"
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const analytics = getAnalytics(app);
-
+const app = !getApps.length ? initializeApp( firebaseConfig) : getApp();
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app)
